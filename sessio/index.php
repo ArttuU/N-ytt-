375266002638fe460277783d8eb41bb1459cbac3
arttu.uuskartano <arthur.newmansion@gmail.com>
2017-02-14 10:40:31 +0200

    päivitys

--- sessio/index.php
+++ sessio/index.php
@@ -10,16 +10,36 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
         <link rel="stylesheet" type="text/css" href="styles.css">
-
     </head>
     <body>
-    <h1>Tervetuloa hienolle sivulle!</h1>
+
+     <div class="container">
+         <div class="row header">
+             <div class="col-md-8">
+                 <p>DYNASTY TIETOPALVELU</p>
+             </div>
+             <div class="col-md-2">
+                 <p>KOKKOLA</p>
+             </div>
+             <div class="col-md-1">
+                 <p>RSS</p>
+             </div>
+             <div class="col-md-1">
+                 <p>Haku</p>
+            </div>
+         </div>
+
+    <div class="row">
+        <div class="col-md-12 nimi">
+            <h1>Tietokantojen hallinta</h1>
+        </div>
+    </div>
     <p>sampletextsampletextsampletext
     sampletextsampletextsampletext
     sampletextsampletextsampletext
     sampletextsampletextsampletext
     sampletextsampletextsampletext</p>
-    <a href="?kill=user">Kirjaudu ulos</a>
+    <a href="?kill=user">KIRJAUDU ULOS</a>
     
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>    
     </body>
