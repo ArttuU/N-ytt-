48887946c413c18de35ac7cfbabd72235dc14a0b
arttu.uuskartano <arthur.newmansion@gmail.com>
2017-02-14 10:30:30 +0200

    päivitys

---
+++ sessio/index.php
@@ -0,0 +1,26 @@
+<?php
+  include("hallinta.php");
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+            <title>index</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+        <link rel="stylesheet" type="text/css" href="styles.css">
+
+    </head>
+    <body>
+    <h1>Tervetuloa hienolle sivulle!</h1>
+    <p>sampletextsampletextsampletext
+    sampletextsampletextsampletext
+    sampletextsampletextsampletext
+    sampletextsampletextsampletext
+    sampletextsampletextsampletext</p>
+    <a href="?kill=user">Kirjaudu ulos</a>
+    
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>    
+    </body>
+</html>
